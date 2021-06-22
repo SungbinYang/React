7713d2c25819550684cf2ab4a8cdd14b944ce5f9
Robert.Yang <ysb05222@naver.com>
2021-06-22 23:12:34 +0900

    Add files via upload

---
+++ react-hooks-example/src/components/Example7.jsx
@@ -0,0 +1,34 @@
+import { useState, useMemo, useCallback } from "react";
+
+function sum(persons) {
+  console.log("sum...");
+  return persons.map((person) => person.age).reduce((l, r) => l + r, 0);
+}
+
+export default function Example7() {
+  const [value, setValue] = useState("");
+  const [persons] = useState([
+    { name: "Sungbin", age: 26 },
+    { name: "Hanna", age: 22 },
+  ]);
+
+  const count = useMemo(() => {
+    return sum(persons);
+  }, [persons]);
+
+  const click = useCallback(() => {
+    console.log(value);
+  }, []);
+
+  return (
+    <div>
+      <input value={value} onChange={change} />
+      <p>{count}</p>
+      <button onClick={click}>click</button>
+    </div>
+  );
+
+  function change(e) {
+    setValue(e.target.value);
+  }
+}
