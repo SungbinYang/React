faab8d2be2b86762037f3ac6eb09c9fc7ceb312a
Robert.Yang <ysb05222@naver.com>
2021-06-23 20:42:36 +0900

    React Router Hooks
    React Router Hooks

--- react-router-example/src/components/LoginButton.jsx
+++ react-router-example/src/components/LoginButton.jsx
@@ -1,12 +1,24 @@
-import { withRouter } from "react-router-dom";
+import { withRouter, useHistory } from "react-router-dom";
 
-export default withRouter(function LoginButton(props) {
-  console.log(props);
+// export default withRouter(function LoginButton(props) {
+//   console.log(props);
+//   function login() {
+//     setTimeout(() => {
+//       props.history.push("/");
+//     }, 1000);
+//   }
+
+//   return <button onClick={login}>로그인 하기</button>;
+// });
+
+export default function LoginButton() {
+  const history = useHistory();
+
   function login() {
     setTimeout(() => {
-      props.history.push("/");
+      history.push("/");
     }, 1000);
   }
 
   return <button onClick={login}>로그인 하기</button>;
-});
+}
