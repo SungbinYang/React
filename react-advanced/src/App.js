0e9cc5781eec630b80620018284fbab43c6be38e
Robert.Yang <ysb05222@naver.com>
2021-06-26 00:03:45 +0900

    createPotal
    createPotal

--- react-advanced/src/App.js
+++ react-advanced/src/App.js
@@ -1,44 +1,77 @@
 import "./App.css";
-import React from "react";
+import { useState } from "react";
+import Modal from "./components/Modal";
 
-const Person = React.memo(({ name, age }) => {
-  console.log("Person render");
-  return (
-    <div>
-      {name} / {age}
-    </div>
-  );
-});
+// const Person = React.memo(({ name, age }) => {
+//   console.log("Person render");
+//   return (
+//     <div>
+//       {name} / {age}
+//     </div>
+//   );
+// });
+
+// function App() {
+//   const [state, setState] = React.useState({
+//     text: "",
+//     persons: [
+//       { id: 1, name: "Robert", age: 26 },
+//       { id: 2, name: "Hanna", age: 22 },
+//     ],
+//   });
+
+//   const toPersonClick = React.useCallback(() => {}, []);
+
+//   const { text, persons } = state;
+//   return (
+//     <div>
+//       <input type="text" value={text} onChange={change} />
+//       <ul>
+//         {persons.map((person) => {
+//           return <Person {...person} key={person.id} onClick={toPersonClick} />;
+//         })}
+//       </ul>
+//     </div>
+//   );
+
+//   function change(e) {
+//     setState({
+//       ...state,
+//       text: e.target.value,
+//     });
+//   }
+// }
 
 function App() {
-  const [state, setState] = React.useState({
-    text: "",
-    persons: [
-      { id: 1, name: "Robert", age: 26 },
-      { id: 2, name: "Hanna", age: 22 },
-    ],
-  });
+  const [visible, setVisible] = useState(false);
 
-  const toPersonClick = React.useCallback(() => {}, []);
+  const open = () => {
+    setVisible(true);
+  };
 
-  const { text, persons } = state;
+  const close = () => {
+    setVisible(false);
+  };
+
   return (
     <div>
-      <input type="text" value={text} onChange={change} />
-      <ul>
-        {persons.map((person) => {
-          return <Person {...person} key={person.id} onClick={toPersonClick} />;
-        })}
-      </ul>
+      <button onClick={open}>Open</button>
+      {visible && (
+        <Modal>
+          <div
+            style={{
+              width: "100vw",
+              height: "100vh",
+              background: "rgba(0, 0, 0, 0.5)",
+            }}
+            onClick={close}
+          >
+            Hello
+          </div>
+        </Modal>
+      )}
     </div>
   );
-
-  function change(e) {
-    setState({
-      ...state,
-      text: e.target.value,
-    });
-  }
 }
 
 export default App;
