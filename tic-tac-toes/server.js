d74234c558695edcd69a36ea4e50c926e1b08b84
Robert.Yang <ysb05222@naver.com>
2021-06-26 03:35:53 +0900

    SSR
    SSR

--- tic-tac-toes/server.js
+++ tic-tac-toes/server.js
@@ -1,10 +1,30 @@
 const express = require('express');
-
 const path = require('path');
+const ReactDOMServer = require('react-dom/server');
+const React = require('react');
+const fs = require('fs');
+
+console.log(
+  ReactDOMServer.renderToString(React.createElement('div', null, 'Hello')),
+);
 
 const app = express();
 
 app.use(express.static(path.join(__dirname, 'build')));
+
+app.get('/test', (req, res) => {
+  const ssr = ReactDOMServer.renderToString(
+    React.createElement('div', null, 'Hello'),
+  );
+  const indexHtml = fs
+    .readFileSync(path.join(__dirname, 'build', 'index.html'))
+    .toString()
+    .replace('<div id="root"></div>', `<div id="root">${ssr}</div>`);
+
+  console.log(indexHtml);
+
+  res.send(indexHtml);
+});
 
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
